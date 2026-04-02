55b25185f5d910dcf6877fbb08979f0c51a77bd8
PonPes Darel Azhar <ponpesdarelazhar.id@gmail.com>
2026-04-02 22:35:56 +0700

    Add cloudinary config structure to resolve Undefined array key cloud error

---
+++ config/cloudinary.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cloudinary Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your Cloudinary settings. Cloudinary is a cloud 
+    | service that offers a solution to a web application's entire image 
+    | management pipeline.
+    |
+    */
+
+    'cloud_url' => env('CLOUDINARY_URL'),
+
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
+
+    'cloud' => [
+        'name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key' => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
+
+];
